16625f8ecb782a9b0959dcfd99c2eef27bbc4f05
Moegamat Yousha Diedericks <diedericksyousha@gmail.com>
2026-03-11 09:17:15 +0200

    added a join

--- SQL/Select.sql
+++ SQL/Select.sql
@@ -7,3 +7,13 @@ FROM krabbycustomers;
 SELECT DISTINCT money_spent, (customer_id)
 FROM krabbycustomers
 ORDER BY money_spent ASC
+
+SELECT e.firstname
+FROM employees e
+INNER JOIN krabbycustomers k
+ON e.firstname = k.firstname
+
+SELECT e.firstname
+FROM employees e
+JOIN krabbycustomers k
+ON e.firstname = k.firstname
